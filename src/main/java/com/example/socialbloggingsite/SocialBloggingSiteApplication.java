84b566e1027c677b9c5c2bcbb28a2a65039f03e1
minhnt <minhnt@nal.vn>
2021-07-30 09:35:07 +0700

    Refactor name

--- src/main/java/com/example/socialbloggingsite/SocialBloggingSiteApplication.java
+++ src/main/java/com/example/socialbloggingsite/SocialBloggingSiteApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SoicialBloggingSiteApplication {
+public class SocialBloggingSiteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SoicialBloggingSiteApplication.class, args);
+		SpringApplication.run(SocialBloggingSiteApplication.class, args);
 	}
 
 }
